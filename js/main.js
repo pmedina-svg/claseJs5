443efcca9eb62f67b1a0a7c7bfbc972b284143cc
pmedina <pia.vmedina@gmail.com>
2026-09-20 22:33:30 -0300

    se agrega switch con opciones seleccionadas

--- js/main.js
+++ js/main.js
@@ -18,8 +18,6 @@ class ViajeEspacial {
     }
 }
 
-const mision = parseInt(prompt("🌎 Tu ubicación actual es: TIERRA \n\nDónde quieres viajar? \n1.Luna \n2.Venus \n3.Marte"));
-
 const viajeLuna = new ViajeEspacial( "Tierra", "Luna", 384400, 40000);
 // prueba de calculo de duración de viaje
 console.log("La duración del viaje a la luna es de " + viajeLuna.calcularDuracion() + " horas.");
@@ -30,5 +28,35 @@ const viajeMarte = new ViajeEspacial( "Tierra", "Marte", 225000000, 40000);
 // prueba de cambio de velocidad
 viajeMarte.cambiarVelocidad(60000);
 console.log("La duración del viaje a Marte es de " + viajeMarte.calcularDuracion() + " horas.");
+
+let mision;
+let viajeSeleccionado;
+
+do{
+    mision = parseInt(prompt("🌎 Tu ubicación actual es: TIERRA \n\nDónde quieres viajar? \n1.Luna \n2.Venus \n3.Marte \n\n0. Se cancela el despegue"));
+
+    switch (mision){
+        case 0:
+            alert("No te preocupes, no todos están listos para volar.");
+            break;
+        case 1:
+            viajeSeleccionado = viajeLuna;
+            console.log(viajeSeleccionado);
+            break;
+        case 2:
+            viajeSeleccionado = viajeVenus;
+            console.log(nombreAstronauta + " eligió viajar a Venus.");
+            break;
+        case 3:
+            viajeSeleccionado = viajeMarte;
+            console.log(nombreAstronauta + " eligió viajar a Marte.");
+            break;
+        default:
+            alert("Opción no válida");
+            console.log(nombreAstronauta + " escribio: " + mision + ". opción no valida.");
+            break;
+    };
+
+}while (mision !== 0);
 
 
